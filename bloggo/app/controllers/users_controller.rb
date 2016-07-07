0bf248403c750009c8ab505298e1eb8a9a01bf8a
Joel Lambdin <email": null>
2016-07-07 17:24:44 -0400

    Complete user signup

--- bloggo/app/controllers/users_controller.rb
+++ bloggo/app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Big Papi Bloggo is very glad you're here, #{@user.name}"
+      redirect_to @user
     else
       render 'new'
     end
