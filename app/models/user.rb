efc39679bc2cb9343d6af0499a97195fe02ea010
Joel_L <plagueoffrogs@gmail.com>
2016-07-10 16:59:47 -0400

    Add follow stats and styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ActiveRecord::Base
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
                                   dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                  foreign_key: "followed_id",
+                                  dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed 
+  has_many :followers, through: :passive_relationships, source: :follower
 
   attr_accessor :remember_token, :activation_token, :reset_token
 
@@ -10,9 +15,9 @@ class User < ActiveRecord::Base
   before_create :create_activation_digest
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true,
-    length: {maximum:255},
-    format: {with: /\A[\w+\-.]+@[a-z\d\-.]+(\.[a-z\d\-]+)+\z/i},
-    uniqueness: {case_sensitive: false}
+                    length: {maximum:255},
+                    format: {with: /\A[\w+\-.]+@[a-z\d\-.]+(\.[a-z\d\-]+)+\z/i},
+                    uniqueness: {case_sensitive: false}
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 
@@ -67,6 +72,19 @@ class User < ActiveRecord::Base
   def feed 
     Post.where("user_id = ?", id)
   end
+
+  def follow(other_user) 
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user) 
+    following.include?(other_user)
+  end
+
 
     private
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -43,3 +43,10 @@ users = User.order(:created_at).take(6)
   end
 end
 
+users = User.all
+user = users.first
+following = users[1..49]
+followers = users[1..39]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
+
