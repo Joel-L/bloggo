8032f09cc8a95756bb42dec6c205808d55ec9691
Joel_L <plagueoffrogs@gmail.com>
2016-07-10 14:48:46 -0400

    Add photo upload capability

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
 
   def create 
     @post = current_user.posts.build(post_params)
@@ -7,15 +8,24 @@ class PostsController < ApplicationController
       flash[:success] = "Grood! Your journey to the Bloggo-side is deepening with each post"
       redirect_to root_url
     else
+      @feed_items = []
       render 'static_pages/home'
     end
   end
 
   def destroy 
+    @post.destroy
+    flash[:success] = "Bloggo...deleted. It was only a fool's hope, as I have been reminded"
+    redirect_to request.referrer || root_url
   end
 
   private 
     def post_params
-      params.require(:post).permit(:content, :title)
+      params.require(:post).permit(:content, :title, :picture)
+    end
+
+    def correct_user
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to root_url if @post.nil?
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -60,16 +60,20 @@ class User < ActiveRecord::Base
     reset_sent_at < 2.hours.ago
   end
 
-  private
-
-  def downcase_email 
-    self.email = email.downcase
+  def feed 
+    Post.where("user_id = ?", id)
   end
 
-  def create_activation_digest 
-    self.activation_token = User.new_token
-    self.activation_digest = User.digest(activation_token)
-  end
+    private
+
+    def downcase_email 
+      self.email = email.downcase
+    end
+
+    def create_activation_digest 
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 
 
 end
